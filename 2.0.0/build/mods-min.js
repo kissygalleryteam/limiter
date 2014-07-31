33a90afb2da8acfb77d796cf8087dfefc5f79d72
弘树 <tiehang.lth@alibaba-inc.com>
2014-07-31 20:31:30 +0800

    sync daily version 2.0.0

---
+++ 2.0.0/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-07-31 8:31:27 PM*/
+KISSY.config("modules",{"kg/limiter/2.0.0/index":{requires:["node","base"]}});
---
+++ 2.0.0/meta/alias.js
@@ -0,0 +1,3 @@
+config({
+    'kg/limiter/index': {alias: ['kg/limiter/2.0.0/index']}
+});
